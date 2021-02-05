d57894862917397b406d39265db205290b4c8d4a
Md. Ishrak Al Hasib Syeam <ishrakalhasib217@gmail.com>
2021-02-05 21:59:22 +0600

    nothing new

--- map-filter.js
+++ map-filter.js
@@ -1,21 +1,21 @@
 const numbers = [3, 4, 5, 6, 7, 8];
 const output = [];
-// for (let i = 0; i < numbers.length; i++) {
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
 
-// numbers.map(function square(element, index, array) {
-//     console.log(element, index, array);
-// })
-// const result = numbers.map(function square(element) {
-//     return element * element;
-// })
-// console.log(result);
+numbers.map(function square(element, index, array) {
+    console.log(element, index, array);
+})
+const result = numbers.map(function square(element) {
+    return element * element;
+})
+console.log(result);
 
-// const result = numbers.map(x => x * x);
-// console.log(result);
+const result = numbers.map(x => x * x);
+console.log(result);
 
 const bigger = numbers.filter(x => x < 3);
 console.log(bigger);
