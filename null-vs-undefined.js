063cc61eeb4fb717858361b71a2f6ac85e5e0add
Md. Ishrak Al Hasib Syeam <ishrakalhasib217@gmail.com>
2021-02-05 21:25:06 +0600

    difference between undefined and null

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(19, 12);
+console.log(result);
+
+const premik = { name: "smart dude", phone: 321321321 };
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
